a633e5cd791c2c3dccb12600700d0d764715a140
Harry YYDS <harry.wang@ielts-daily.com>
2026-07-15 17:46:40 +1000

    add createDate in get-job-status.js

--- src/automation/tasks/get-job-status.js
+++ src/automation/tasks/get-job-status.js
@@ -13,11 +13,26 @@ export async function getJobStatus(businessPage) {
     const status = await rightFrame.locator('#STATUS').inputValue();
     await businessPage.waitForTimeout(1000);
     const reason = await rightFrame.locator('select[name="REASON"]').inputValue();
+    //extract create date
+    const jobInfoText = await rightFrame
+        .locator('span[title*="Object ID // Wty Bill No // ASC Job No // Create Date"]')
+        .textContent() || '';
 
+    const cleanedJobInfoText = jobInfoText
+        .replace('[', '')
+        .replace(']', '')
+        .replace(/&nbsp;/g, ' ') // 如果读取的是 innerHTML 才需要，textContent 通常不用
+        .trim();
 
+    const values = cleanedJobInfoText
+        .split('//')
+        .map(v => v.trim());
+
+    const createDate = values[3];
     return {
         success: true,
         status,
-        reason
+        reason,
+        createDate
     };
 }
